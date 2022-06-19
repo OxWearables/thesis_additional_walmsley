fbbd7d0cdf3eaaebc8ba4b0a67c09d22e272b07d
R-Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2022-06-19 09:11:07 +0100

    add statistical test for interaction with health status

---
+++ chapter6_qba/additional-sensitivity-analyses/thesis_interact_health_status.R
@@ -0,0 +1,52 @@
+interact_health_status <- function(rot_list, act_vars_z, df){
+ 
+  for (i in 1:length(rot_list)) {
+    # set up
+    comp_labels <- rot_list[[i]]
+    comp_name <- names(rot_list)[i]
+    
+    # do transformation
+    piv_c <-
+      as.data.frame(robCompositions::pivotCoord(act_vars_z[, comp_labels]))
+    colnames(piv_c) <- c("piv1", "piv2", "piv3")
+
+    # make new data with these variables
+    loc <- cbind(df, piv_c)
+
+    # basic cox model
+    cox_mod_simple <- coxph(
+      Surv(age_entry, age_exit, CVD_event) ~
+        strata(sex) + ethnicity + smoking + alcohol +
+        fruit_and_veg_cats + oily_fish +
+        red_and_processed_meat_cats + education_cats +
+        TDI_quartiles +
+        piv1 + piv2 + piv3 + bad_health,
+      data = loc
+    )
+    
+    # model with interaction
+    cox_mod_interact <- coxph(
+      Surv(age_entry, age_exit, CVD_event) ~
+        strata(sex) + ethnicity + smoking + alcohol +
+        fruit_and_veg_cats + oily_fish +
+        red_and_processed_meat_cats + education_cats +
+        TDI_quartiles +
+        piv1*bad_health + piv2*bad_health + piv3*bad_health,
+      data = loc
+    )
+    
+    print(anova(cox_mod_interact, cox_mod_simple))
+    print(comp_name)
+    print(summary(cox_mod_interact)$coef)
+  }
+}
+
+
+
+
+
+
+
+
+
+
